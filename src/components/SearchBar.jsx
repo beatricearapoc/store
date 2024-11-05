b447308185fc8ebb517e2c1e138113d557e474f6
beatricearapoc <beatricearapoc@gmail.com>
2024-11-05 19:35:38 +0800

    Added Tests (SearchBar, Button)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,13 +4,13 @@ const SearchBar = ({ query, setQuery, stockChecked, setStockChecked, sort, setSo
   return (
     <div>
         <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Type here"
-        className='border-2 rounded-lg p-2 border-pink-200'
+        className='border-2 rounded-lg p-2 border-pink-200' data-testid="search-text"
         />
         <div>
-            <input type="checkbox" checked={stockChecked} onChange={(e) => setStockChecked(e.target.checked)}/>
+            <input type="checkbox" checked={stockChecked} onChange={(e) => setStockChecked(e.target.checked)} data-testid="search-checkbox"/>
             Only show products in stock
         </div>
-        <select value = {sort} onChange={(e) => setSort(e.target.value)}>
+        <select value = {sort} onChange={(e) => setSort(e.target.value)} data-testid="search-select">
           <option value="asc">asc</option>
           <option value="desc">desc</option>
         </select>
